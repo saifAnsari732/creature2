54baef36ecd4ec899baf2670101e5b480f3ba057
saifAnsari732 <ansarisaifuddin732@gmail.com>
2025-12-09 15:55:43 +0530

    first commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './Home.css';
+
 import machineryImage from '../public/mach1.jpg';
 import machineryImage2 from '../public/mach2.jpeg';
 import machineryImage3 from '../public/mach3.jpeg';
 import machineryImage5 from '../public/mach5.avif';
-// import machineryImage6 from '../public/mach6.avif';
-import machineryvideo from '../public/food-video.mp4';
+import machineryImage6 from '../public/mach6.avif';
+import machineryImage7 from '../public/mach7.png';
+import machineryImage8 from '../public/mach8.jpg';
+import machineryImage9 from '../public/mach9.avif';
+import machineryImage10 from '../public/mach10.jpg';
+
+
 import Footer from './Footer';
 import MobileNavbar from './Navigation';
 import CallButton from './Call';
 import WhatsAppButton from './whatsapp';
 import { FaSearch } from "react-icons/fa";
 // VIDEO ICON
+import machineryvideo from '../public/food-video.mp4';
+
 import v1 from '../public/videoicon/big-sale.mp4';
 import v2 from '../public/videoicon/sale.mp4';
 import v3 from '../public/videoicon/sales1.mp4';
@@ -230,6 +238,192 @@ function Home() {
                     </div>
 
                     {/* Card 6 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage2} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v3} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 6 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage6} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v3} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 7 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage7} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v4} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 8 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage8} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v2} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 9 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage9} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v5} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 10 */}
+                    <div className="beautiful-card">
+                        <div className="card-image-wrapper">
+                            <img src={machineryImage10} alt="Industrial Robot" />
+                            {/* <span className="discount-badge">10% OFF</span> */}
+                               <video 
+                            className='discount-badge h-12 rounded-full '
+                            src={v1} 
+                              autoPlay 
+                              loop 
+                              muted
+                             playsInline >
+                    </video>
+                        </div>
+                        <div className="card-content">
+                            <h3>Heavy Duty Industrial Robot</h3>
+                            <p className="card-description">Robotic system for manufacturing</p>
+                            <div className="card-rating">
+                                <span className="stars">★★★★★</span>
+                                <span className="rating-text">(125)</span>
+                            </div>
+                            <div className="card-price">
+                                <span className="original-price">₹99,999</span>
+                                <span className="sale-price">₹61,999</span>
+                            </div>
+                            <div className="card-actions">
+                                <CallButton />
+                                <WhatsAppButton />
+                            </div>
+                        </div>
+                    </div>
+                     {/* Card 6 */}
                     <div className="beautiful-card">
                         <div className="card-image-wrapper">
                             <img src={machineryImage2} alt="Industrial Robot" />
